be98667cd59bdbc451317cad0733f715408696f1
SAP HANA Academy <hanaacademy@sap.com>
2021-10-29 15:51:56 +0200

    Refresh existing SAP HANA Cloud schema or HDI Container

---
+++ generators/hdi/index.js
@@ -0,0 +1,50 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const hanaUtils = require('../app/hanaUtils');
+
+module.exports = class extends Generator {
+    prompting() {
+        this.log("");
+        if (this.config.get("hanaTargetHDI") !== "") {
+            this.log("Refreshing existing SAP HANA Cloud HDI Container:", this.config.get("hanaTargetHDI"));
+        } else {
+            this.log("No existing SAP HANA Cloud HDI Container was defined for this project.");
+            return;
+        }
+        this.log("");
+        return this.prompt([
+            {
+                type: "confirm",
+                name: "confirm",
+                message: "The following project files will be replaced: db/src/" + this.config.get("hanaTargetHDI") + ".hdbgrants, db/src/" + this.config.get("hanaTargetHDI") + ".hdbsynonym, db/cfg/" + this.config.get("hanaTargetHDI") + ".hdbsynonymconfig, db/src/" + this.config.get("projectName").replace(/\-/g, "_").toUpperCase() + "_DB_" + this.config.get("hanaTargetHDI").replace(/\-/g, "_").toUpperCase() + "_*.hdbview, db/" + this.config.get("hanaTargetHDI") + ".cds, srv/" + this.config.get("hanaTargetHDI") + "-service.cds. Are you absolutely sure?",
+                default: false
+            },
+        ]).then((answers) => {
+            this.config.set(answers);
+        });
+    }
+
+    async writing() {
+        var answers = this.config;
+        if (answers.get("confirm")) {
+            hanaUtils.targetHDI2Schema(this, answers);
+            hanaUtils.processSchema(this, answers);
+            this.log("");
+            this.log("Project files have been replaced.");
+            this.log("");
+        } else {
+            this.log("");
+            this.log("Project files have not been replaced.");
+            this.log("");
+        }
+        answers.delete('hanaEndpoint');
+        answers.delete('hanaUser');
+        answers.delete('hanaPassword');
+        answers.delete('confirm');
+    }
+
+    end() {
+        this.log("");
+    }
+
+};
---
+++ generators/schema/index.js
@@ -0,0 +1,66 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const hanaUtils = require('../app/hanaUtils');
+
+module.exports = class extends Generator {
+    prompting() {
+        this.log("");
+        if (this.config.get("schemaName") !== "" && this.config.get("hanaTargetHDI") === "") {
+            this.log("Refreshing existing SAP HANA Cloud schema:", this.config.get("schemaName"));
+        } else {
+            this.log("No existing SAP HANA Cloud schema was defined for this project.");
+            return;
+        }
+        this.log("");
+        return this.prompt([
+            {
+                type: "input",
+                name: "hanaEndpoint",
+                message: "What is your SAP HANA Cloud SQL endpoint?",
+                default: "<guid>.hana.<region>.hanacloud.ondemand.com:443"
+            },
+            {
+                type: "input",
+                name: "hanaUser",
+                message: "What is your SAP HANA Cloud user name?",
+                default: "DBADMIN"
+            },
+            {
+                type: "password",
+                name: "hanaPassword",
+                message: "What is the password for your SAP HANA Cloud user?",
+                mask: "*",
+                default: ""
+            },
+            {
+                type: "confirm",
+                name: "confirm",
+                message: "The following project files will be replaced: db/src/" + this.config.get("schemaName") + ".hdbgrants, db/src/" + this.config.get("schemaName") + ".hdbsynonym, db/src/" + this.config.get("schemaName") + "-*.hdbview, db/" + this.config.get("schemaName") + ".cds, srv/" + this.config.get("schemaName") + "-service.cds. Are you absolutely sure?",
+                default: false
+            },
+        ]).then((answers) => {
+            this.config.set(answers);
+        });
+    }
+
+    async writing() {
+        var answers = this.config;
+        this.log("");
+        if (answers.get("confirm")) {
+            hanaUtils.processSchema(this, answers);
+            this.log("Project files have been replaced.");
+        } else {
+            this.log("Project files have not been replaced.");
+        }
+        this.log("");
+        answers.delete('hanaEndpoint');
+        answers.delete('hanaUser');
+        answers.delete('hanaPassword');
+        answers.delete('confirm');
+    }
+
+    end() {
+        this.log("");
+    }
+
+};
