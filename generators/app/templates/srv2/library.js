beb45f2cdf519d08ce5bf289a8257372c1cf8ba7
SAP HANA Academy <hanaacademy@sap.com>
2022-03-10 18:52:25 +0100

    Additional Service

--- generators/app/templates/srv2/library.js
+++ generators/app/templates/srv2/library.js
@@ -15,19 +15,49 @@ async function getSMInstance(sm, tenantId) {
             }
         };
         let res = await axios(options);
-        // get service binding details
+        // get service offering id
         let options1 = {
             method: 'GET',
-            url: sm.sm_url + "/v1/service_bindings?labelQuery=tenant_id eq '" + tenantId + "'",
+            url: sm.sm_url + "/v1/service_offerings?fieldQuery=catalog_name eq 'hana'",
             headers: {
                 'Authorization': 'Bearer ' + res.data.access_token
             }
         };
         let res1 = await axios(options1);
         if (res1.data.num_items === 1) {
-            return res1.data.items[0];
+            // get service plan id
+            let options2 = {
+                method: 'GET',
+                url: sm.sm_url + "/v1/service_plans?fieldQuery=catalog_name eq 'hdi-shared' and service_offering_id eq '" + res1.data.items[0].id + "'",
+                headers: {
+                    'Authorization': 'Bearer ' + res.data.access_token
+                }
+            };
+            let res2 = await axios(options2);
+            if (res2.data.num_items === 1) {
+                // get service binding details
+                let options3 = {
+                    method: 'GET',
+                    url: sm.sm_url + "/v1/service_bindings?labelQuery=service_plan_id eq '" + res2.data.items[0].id + "' and tenant_id eq '" + tenantId + "'",
+                    headers: {
+                        'Authorization': 'Bearer ' + res.data.access_token
+                    }
+                };
+                let res3 = await axios(options3);
+                if (res3.data.num_items === 1) {
+                    return res3.data.items[0];
+                } else {
+                    let errmsg = { 'error': 'Service binding not found for tenant ' + tenantId };
+                    console.log(errmsg);
+                    return errmsg;
+                }
+            } else {
+                let errmsg = { 'error': 'Service plan hdi-shared not found' };
+                console.log(errmsg);
+                return errmsg;
+            }
         } else {
-            let errmsg = { 'error': 'Service binding not found for tenant ' + tenantId };
+            let errmsg = { 'error': 'Service offering hana not found' };
             console.log(errmsg);
             return errmsg;
         }
